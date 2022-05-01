13007ef2a2171c56ae81db908a3793953a8f8e2e
tamirmatok <tamirmatok123@gmail.com>
2022-05-01 18:37:27 +0300

    Add quickSort function
    Signed-off-by: tamirmatok <tamirmatok123@gmail.com>

---
+++ quick_sort.h
@@ -0,0 +1,58 @@
+#ifndef QUICK_SORT_h
+#define QUICK_SORT_h
+#include <iostream>
+using namespace std;
+
+template<class T>
+int partition(T arr[], int start, int end)
+{
+    int pivot = arr[start];
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+
+    while (i < pivotIndex && j > pivotIndex) {
+
+        while (arr[i] <= pivot) {
+            i++;
+        }
+
+        while (arr[j] > pivot) {
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+
+    return pivotIndex;
+}
+
+template<class T>
+void quickSort(T arr[], int start, int end)
+{
+
+    // base case
+    if (start >= end)
+        return;
+
+    // partitioning the array
+    int p = partition(arr, start, end);
+
+    // Sorting the left part
+    quickSort(arr, start, p - 1);
+
+    // Sorting the right part
+    quickSort(arr, p + 1, end);
+}
+
+#endif
